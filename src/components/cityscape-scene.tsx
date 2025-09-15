1ca854d665e690e7b30d152503bb2a7755d79c56
Samarth <samarthnarayankar1@gmail.com>
2025-09-15 11:10:02 +0000

    Undo changes just now done

--- src/components/cityscape-scene.tsx
+++ src/components/cityscape-scene.tsx
@@ -3,7 +3,7 @@
 
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
-import React, { useMemo, useRef } from 'react';
+import { useMemo, useRef } from 'react';
 
 function Buildings() {
     const buildings = useMemo(() => {
@@ -63,7 +63,7 @@ function CameraController() {
         scrollY.current = window.scrollY;
     };
 
-    React.useEffect(() => {
+    useEffect(() => {
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
